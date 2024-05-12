f8016916962c6d5538a72180408e29b32a176533
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-12 15:59:53 +0530

    Add files via upload

---
+++ C/Assignment 24 - Handling Multiple Strings/Que_01.c
@@ -0,0 +1,20 @@
+
+              /* Que - Write a function to store strings, taken from user, to the given 2d char array */
+
+              #include<stdio.h>
+              void scanning(y[2][3],int size);
+              int main()
+              {
+                  char A[2][4];
+                  scanning(A[2][3] , 4);
+
+                  displaying(A, 4);
+
+
+
+              }
+
+              void scanning(y[2][3],int size)
+              {
+                
+              }
