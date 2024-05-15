ab9ec4738dce44e8e1c25969e7971b577a53083e
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-15 17:30:39 +0530

    Add files via upload

---
+++ C++/Assignment 29 - Basics Of C++/Que_01.cpp
@@ -0,0 +1,14 @@
+   
+    /* Que - Write a C++ program to print Hello MySirG on the screen. */
+
+    #include<iostream>
+
+    using namespace std;
+
+    int main()
+
+        {
+             cout<<"Hello MySirG";
+
+             return 0;
+        }
