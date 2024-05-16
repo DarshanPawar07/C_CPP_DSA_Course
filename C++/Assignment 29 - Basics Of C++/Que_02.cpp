5b5e0ada74a3369318e806ea0472447ab3a7c4bb
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-16 17:04:15 +0530

    Add files via upload

---
+++ C++/Assignment 29 - Basics Of C++/Que_02.cpp
@@ -0,0 +1,14 @@
+
+    /* Que - Write a C++ program to print Hello on the first line and MySirG on the second line using endl.*/
+    
+    #include<iostream>
+
+    using namespace std;
+
+    int main()
+
+        {
+             cout<<"Hello "<<endl<<"MySirg";
+
+             return 0;
+        }
