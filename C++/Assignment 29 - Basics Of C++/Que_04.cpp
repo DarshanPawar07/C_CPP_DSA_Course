06db1908385edb5d0fa193007ec599e1067316f8
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-18 17:31:07 +0530

    Add files via upload

---
+++ C++/Assignment 29 - Basics Of C++/Que_04.cpp
@@ -0,0 +1,24 @@
+
+     /* Que - Write a C++ program to calculate the area of a circle */
+
+     #include<iostream>
+     using namespace std;
+
+     
+     int main()
+
+                { 
+                    int r;
+
+                    cout<<"Enter The Radius Of The Circle";
+                    cout<<"\n";
+
+                    cin>>r;
+
+                    cout<<"The Area Of The Circle Of Radius "<<r<<"  Is " <<(3.14* r*r);
+
+                    cout<<"\n";
+                    
+                    return 0;
+
+                 }
