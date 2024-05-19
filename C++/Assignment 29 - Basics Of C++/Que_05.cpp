d62d4ae57d57d57afaae696f61f13d0d47bfc848
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-19 21:26:28 +0530

    Add files via upload

---
+++ C++/Assignment 29 - Basics Of C++/Que_05.cpp
@@ -0,0 +1,30 @@
+
+   /* Que- Write a C++ program to calculate the volume of a cuboid. */ 
+
+     
+     #include<iostream>
+
+     using namespace std;
+     
+     int main()
+
+              { 
+                   int l ,b , h ;
+
+                   cout<<"Enter Length Of Cuboid";
+                   cout<<"\n";
+                   cin>>l;
+                  
+                   cout<<"Enter The Breadth Of Cuboid";
+                   cout<<"\n";
+                   cin>>b;
+
+                   cout<<"Enter The Height Of The Cuboid";
+                   cout<<"\n";
+                   cin>>h;
+
+                   cout<<"The Volume Of Cuboid Is "<<l*b*h;
+
+                   return 0;
+
+               }  
