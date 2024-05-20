f27bcd5c5495ae8f532b5a27a48f82410dbd2bd6
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-20 20:30:34 +0530

    Add files via upload

---
+++ C++/Assignment 29 - Basics Of C++/Que_06.cpp
@@ -0,0 +1,19 @@
+
+     /* Que - . Write a C++ program to calculate an average of 3 numbers. */
+
+     #include<iostream>
+     using namespace std;
+
+     int main()
+     {
+        int a,b,c;
+
+        cout<<"Enter Three Numbers";
+        cout<<"\n";
+
+        cin>>a>>b>>c;
+
+        cout<<"The Average Of "<<a<<","<<b<<" And "<<c<<" Is " <<((a+b+c)/3);
+
+        return 0;
+     }
