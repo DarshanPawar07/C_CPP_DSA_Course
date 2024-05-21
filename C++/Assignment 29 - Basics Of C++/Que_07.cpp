70f7827bb67dea42c80007380136311063a00926
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-21 15:37:57 +0530

    Add files via upload

---
+++ C++/Assignment 29 - Basics Of C++/Que_07.cpp
@@ -0,0 +1,18 @@
+
+     /* Que - Write a C++ program to calculate the square of a number */
+
+     #include<iostream>
+     using namespace std;
+
+     int main()
+     {
+         int a;
+         cout<<"Enter A Number";
+         cout<<endl;
+         cin>>a;
+
+         cout<<"The Square Of "<<a<<" Is " <<(a*a);
+
+         return 0;
+
+     }
