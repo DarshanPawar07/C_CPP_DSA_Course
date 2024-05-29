3b512a986d0cb06fdd5a282f35c127f701376f0f
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-05-29 19:35:15 +0530

    Add files via upload

---
+++ C++/Assignment 30 - Functions In C++/Que_05.cpp
@@ -0,0 +1,25 @@
+
+     /* Que - Define a function to check whether a given number is a term in a Fibonacci series or not.  */
+
+
+     #include<iostream>
+     using namespace std;
+
+    
+           /*   bool isInFib(int num)
+                                     {
+                                          int a=-1,b=1,c=0;
+
+                                          while(c<num)
+                                                       {
+                                                             c=a+b;
+                                                             if(num==c)
+                                                             return true;
+                                                             a=b;
+                                                             b=c;
+                                                        }
+
+                                          return false;
+                                      }
+
+                                      */ 
