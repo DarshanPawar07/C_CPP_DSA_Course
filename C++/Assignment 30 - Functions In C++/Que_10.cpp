67c1a736d206b3a1d40a373d3fbd53ade3ea4f11
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-06-04 10:48:22 +0530

    Add files via upload

---
+++ C++/Assignment 30 - Functions In C++/Que_10.cpp
@@ -0,0 +1,21 @@
+
+     /* Que -  Write functions using function overloading to add two numbers having different data types  */
+
+
+     #include<iostream>
+     using namespace std;
+
+     
+      int add(int a,int b)
+                           {
+                               return a+b;
+                           } 
+    
+     int add(float a,float b)
+                           {
+                                return a+b;
+                           }
+     int add(double a,double b)
+                           {
+                                return a+b;
+                           }
