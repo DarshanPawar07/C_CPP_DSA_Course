de7877d3aca6f22f996cd3d0998ca48cafcb1fa8
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-06-15 19:37:49 +0530

    Add files via upload

---
+++ C++/Assignment 31 - More On Functions In C++/Que_09.cpp
@@ -0,0 +1,29 @@
+
+       /* Que 9 - In question 1, define a method to delete a specific node. */
+
+       #include<iostream>
+       #include<stdio.h>
+       using namespace std;
+       
+
+       struct node
+       {
+             int item;
+             int *next;
+       };
+
+       class SLL
+         {    
+            private:
+              node *start;
+            public:
+              void delete_specific();
+
+         };
+        
+        void SLL::delete_specific()
+          {
+            
+          }
+        
+         
