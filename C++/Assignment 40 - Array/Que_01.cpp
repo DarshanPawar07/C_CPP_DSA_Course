cecbfeaa9fc7f2c8c42fe491e7cf019c5f19b6c3
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-09-05 14:39:43 +0530

    Add files via upload

---
+++ C++/Assignment 40 - Array/Que_01.cpp
@@ -0,0 +1,18 @@
+
+        /* Que-1 - Define a class Array to implement array data structure with member variables to 
+                   store capacity of array, last index of the last filled block of the array and a pointer
+                   to hold the address of the first block of the dynamically created array. */
+
+        #include<iostream>
+        using namespace std;
+
+        class array 
+        {
+             private:  
+                 int capacity;
+                 int last_index;
+                 int *p;
+             
+             public:
+                 
+        };
