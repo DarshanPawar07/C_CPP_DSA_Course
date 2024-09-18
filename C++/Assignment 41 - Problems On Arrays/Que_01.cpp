0bba8336f4cb19a7e1ef1343eafc91ba99991373
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-09-18 21:33:04 +0530

    Add files via upload

---
+++ C++/Assignment 41 - Problems On Arrays/Que_01.cpp
@@ -0,0 +1,25 @@
+
+      /* Que 1 - Define a function to sort elements of the array */
+
+      #include<iostream>
+      #include<stdio.h>
+      using namespace std;
+
+      class Array
+              {    
+                   private:
+                         int capacity;
+                         int last_index;
+                         int *p;
+                   
+                   public:
+                        void sort();
+
+              };
+
+     void Array::sort()
+               {
+                  
+                   
+                   
+               }
