1d66955825c53a97253582f1a6aadc49a8a37b95
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-10-04 19:41:36 +0530

    Add files via upload

---
+++ C++/Assignment 42 - Dynamic Arrays/Que_05.cpp
@@ -0,0 +1,24 @@
+   
+       /* Que 5 - In question 1, define a method which returns the current capacity of the array. */
+
+       #include<stdio.h>
+       #include<iostream>
+       using namespace std;  //incomplete
+
+
+       class DynArray
+          {
+               private:
+                   int capacity;
+                   int last_index;
+                   int *p;
+                
+                public:
+                     void current_capacity();
+
+          };
+
+        void DynArray::current_capacity()
+            {
+                
+            }
