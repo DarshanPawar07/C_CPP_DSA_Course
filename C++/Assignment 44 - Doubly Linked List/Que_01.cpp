681e0b022217814adfa551db5d4787d99630627a
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-11-04 17:10:48 +0530

    Add files via upload

---
+++ C++/Assignment 44 - Doubly Linked List/Que_01.cpp
@@ -0,0 +1,27 @@
+
+
+       /* Que 1 - Define a class DLL to implement singly linked list data structure with member variable start 
+                  pointer of type node */
+
+
+       #include<iostream>
+       using namespace std;
+         
+       struct node
+         {
+            int item;
+            node *prev;
+            node *next;
+
+         };
+
+       class DLL
+         {    
+            private:
+              node *start;
+
+            public:
+            
+
+
+         };
