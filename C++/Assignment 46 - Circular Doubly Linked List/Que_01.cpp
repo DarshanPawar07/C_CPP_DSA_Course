9a8a779eb14cb980fc2084862ba602efab1ecf2c
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-12-03 18:15:56 +0530

    Add files via upload

---
+++ C++/Assignment 46 - Circular Doubly Linked List/Que_01.cpp
@@ -0,0 +1,22 @@
+
+       /* Que 1 - Define a class CDLL to implement Circular Doubly linked list data structure with 
+                  member variable start pointer of type node. */
+
+       #include<iostream>
+       using namespace std;
+
+       struct node
+          {
+               int item;
+               node *prev;
+               node *next;
+          };
+        
+        class CDLL
+           {
+                private:
+                    node *start;
+                
+                 public:
+                    
+           };
