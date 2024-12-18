320670b039400e18bec84aef5c7f67065a11e187
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-12-18 19:40:03 +0530

    Add files via upload

---
+++ C++/Assignment 47 - Stack Using Arrays/Que_01.cpp
@@ -0,0 +1,18 @@
+
+      /* Que 1 - Define a class Stack with capacity, top and ptr pointer as member variables. Implement stack 
+                 using array */
+      
+      #include<iostream>
+      #include<stdio.h>
+      using namespace std;
+
+      class Stack
+        {   
+            private:
+               int capacity;
+               int top;
+               int *ptr;
+
+            public:
+                 
+        };
