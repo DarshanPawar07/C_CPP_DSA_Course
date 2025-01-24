6682ed71c24444aa00c3dcc73ff6eab67aec974a
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2025-01-24 23:27:17 +0530

    Add files via upload

---
+++ C++/Assignment 50 - Queue Using Linked List/Que_01.cpp
@@ -0,0 +1,18 @@
+
+    /* Que 1 - Define a class Queue with capacity, front, rear and ptr pointer as member variables.
+                Implement queue using array.*/
+
+    #include<iostream>
+    #include<stdio.h>
+    using namespace std;
+
+    class Queue
+      { 
+            private:
+                int capacity;
+                int front;
+                int rear;
+                int *ptr;
+           
+           public:
+      }; 
