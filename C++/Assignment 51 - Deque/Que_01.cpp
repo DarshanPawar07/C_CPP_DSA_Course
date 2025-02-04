37c75926672baf195b1cd852e16fc864c615eda8
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2025-02-04 23:01:19 +0530

    Add files via upload

---
+++ C++/Assignment 51 - Deque/Que_01.cpp
@@ -0,0 +1,25 @@
+
+        /* Que 1 -  Define a class Deque with node type pointers front and rear as member variables. 
+                    Implement Deque using doubly linked list.*/
+        
+        #include<iostream>
+        using namespace std;
+       
+        struct node
+          {
+               int item;
+               int *next;
+               int *prev;
+          };
+        
+        class Deque
+           {
+              
+                private: 
+                     node *front;
+                     node *rear;
+                
+                public:
+                    
+           };
+           
