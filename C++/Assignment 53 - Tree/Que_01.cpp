a303c4c4087ec73b1c215f07e603a31af24fab30
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2025-03-06 23:45:36 +0530

    Add files via upload

---
+++ C++/Assignment 53 - Tree/Que_01.cpp
@@ -0,0 +1,21 @@
+
+        /* Que 1 - Define a class BST (Binary Search Tree) with node type pointer root as member variable. 
+                   Implement Binary Search Tree using linked representation.  */
+        
+        #include<iostream>
+        using namespace std;
+
+        struct node
+          {
+               int item;
+               node *left;
+               node *right;
+          };
+        
+        class BST
+          {
+                private:
+                    node *root;
+                
+                public:
+          };
