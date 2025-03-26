e26284e75ab5d88e6a22ec91f12136981f1179c4
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2025-03-26 06:05:16 +0530

    Add files via upload

---
+++ C++/Assignment 54 - AVL Tree/Que_01.cpp
@@ -0,0 +1,16 @@
+
+        /* Que 1 - Define a class Graph using matrix representation with v_count, e_count and adj pointer 
+                    as instance members.*/
+        
+        #include<iostream>
+        using namespace std;
+
+        
+        class Graph
+          {
+                private:
+                     int v_count,e_count;
+                     int **adj;
+                
+                public:
+          };
