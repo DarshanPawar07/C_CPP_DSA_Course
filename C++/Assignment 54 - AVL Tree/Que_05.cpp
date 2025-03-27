79d33bd6798a0491fab7fd6c40fe319d983490cc
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2025-03-27 23:48:43 +0530

    Add files via upload

---
+++ C++/Assignment 54 - AVL Tree/Que_05.cpp
@@ -0,0 +1,27 @@
+
+        /* Que 5 -In question 1, define a method rightRotate for clockwise rotation */
+        
+        #include<iostream>
+        using namespace std;
+
+        struct node
+          {
+               int item;
+               node *left;
+               node *right;
+               int height;
+          };
+        
+        class AVL
+          {
+                private:
+                    node *root;
+                
+                public:
+                    void clockwise_rotation();
+          };
+        
+        void AVL::clockwise_rotation()
+          {
+               
+          }
