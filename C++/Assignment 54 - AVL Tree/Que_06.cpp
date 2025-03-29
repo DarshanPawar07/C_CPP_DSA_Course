81390a063a34336da5df9d34b2fda470fddeef62
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2025-03-29 22:13:13 +0530

    Add files via upload

---
+++ C++/Assignment 54 - AVL Tree/Que_06.cpp
@@ -0,0 +1,27 @@
+
+        /* Que 6 - In question 1, define a method to insert new element in the tree  */
+        
+        #include<iostream>
+        using namespace std;
+
+        struct node
+          {
+               int item;
+               node *left;
+               node *right;
+               int height;
+          };
+        
+        class AVL
+          {
+                private:
+                    node *root;
+                
+                public:
+                    void insert(int);
+          };
+
+        void insert(int data)
+         {
+              
+         }
